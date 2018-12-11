1af59dd709091fd5f2b3ea10b3d7209ece221353
thomas-j92 <thomas.j@impactus.co.uk>
2018-12-11 22:20:40 +0000

    start on components

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,34 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('person-attending', {
+	template: `
+	<div id="attending">
+		<h2>Attending</h2>
+		<ul><person v-for="user in users" v-bind:name="user.name" v-if="user.attending" v-bind:key="user.id">{{ user.name }}</person></ul>
+	</div>
+	`,
+
+	data() {
+		return {
+			users: [
+	            {id: 1, name: "Tommy", rsvp: true, attending: true},
+	            {id: 2, name: "Jessie", rsvp: true, attending: true},
+	            {id: 3, name: "Aaron", rsvp: false, attending: false},
+	            {id: 4, name: "Yasmin", rsvp: false, attending: false},
+	        ]
+		}
+	}
+});
+
+Vue.component('person', {
+	props: ['name'],
+	template: `<li><slot></slot> {{ name }} <button>Edit</button></li>`
+})
+
+// Vue.component('modal', {
+// 	template: 
+// })
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,6 +55,48 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-// const app = new Vue({
-//     el: '#app'
-// });
+var root = new Vue({
+    el: '#root',
+    // data: {
+    //     user_name: '',
+    //     users: [
+    //         {name: "Tommy", rsvp: true, attending: true},
+    //         {name: "Jessie", rsvp: true, attending: true},
+    //         {name: "Aaron", rsvp: false, attending: false},
+    //         {name: "Yasmin", rsvp: false, attending: false},
+    //     ],
+    //     className: "red",
+    //     isLoading: false
+    // },
+    // methods: {
+    //     addNames: function() {
+    //         this.users.push({
+    //             name: this.user_name,
+    //             rsvp: false,
+    //             attending: false
+    //         });
+
+    //         this.user_name = '';
+    //     },
+    //     markAsAttending: function(index) {
+    //         this.users[index].attending = true;
+    //     }
+    // },
+    // computed: {
+    //     reversedMessage: function() {
+    //         return this.title.split("").reverse().join('');
+    //     },
+    //     attending: function() {
+    //         return this.users.filter(user => user.attending);
+    //     }
+    // },
+    // mounted() {
+    //     document.querySelector('#add_user_btn').addEventListener('click', () => {
+    //         var input = document.querySelector('#add_user_input');
+
+    //         root.users.push(input.value);
+
+    //         input.value = '';
+    //     })
+    // }
+})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,12 @@
         <title>WeddingInviter</title>
 
         <link href="{{ asset("css/app.css") }}" rel="stylesheet" type="text/css">
-        <script type="text/javascript" src="{{ asset("js/app.js") }}"></script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             <div id="root">
-                <h1>All users</h1>
-                <ul v-bind:class="{'red' : isLoading}">
+                <person-attending></person-attending>
+                {{-- <ul v-bind:class="{'red' : isLoading}">
                     <li v-for="(user, index) in users">
                         @{{user.name}}
                         <div v-if="! user.attending">
@@ -31,56 +30,13 @@
 
                 <h1>Add New Attendee</h1>
                 <input type="text" name="add_user" id="add_user_input" v-model="user_name">
-                <button v-on:click="addNames" id="add_user_btn">Add</button>
+                <button v-on:click="addNames" id="add_user_btn">Add</button> --}}
             </div>
         </div>
 
+        <script type="text/javascript" src="{{ asset("js/app.js") }}"></script>
         <script>
-            var root = new Vue({
-                el: '#root',
-                data: {
-                    user_name: '',
-                    users: [
-                        {name: "Tommy", rsvp: true, attending: true},
-                        {name: "Jessie", rsvp: true, attending: true},
-                        {name: "Aaron", rsvp: false, attending: false},
-                        {name: "Yasmin", rsvp: false, attending: false},
-                    ],
-                    className: "red",
-                    isLoading: false
-                },
-                methods: {
-                    addNames: function() {
-                        this.users.push({
-                            name: this.user_name,
-                            rsvp: false,
-                            attending: false
-                        });
-
-                        this.user_name = '';
-                    },
-                    markAsAttending: function(index) {
-                        this.users[index].attending = true;
-                    }
-                },
-                computed: {
-                    reversedMessage: function() {
-                        return this.title.split("").reverse().join('');
-                    },
-                    attending: function() {
-                        return this.users.filter(user => user.attending);
-                    }
-                },
-                mounted() {
-                    // document.querySelector('#add_user_btn').addEventListener('click', () => {
-                    //     var input = document.querySelector('#add_user_input');
-
-                    //     root.users.push(input.value);
-
-                    //     input.value = '';
-                    // })
-                }
-            })
+            
         </script>
     </body>
 </html>
