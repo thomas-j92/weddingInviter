527bad8435ff84db236db09d82b6812875404dbd
thomas-j92 <thomas.j@impactus.co.uk>
2019-02-17 12:45:22 +0000

    start on using datatables

--- resources/views/admin/guests.blade.php
+++ resources/views/admin/guests.blade.php
@@ -5,15 +5,30 @@
 @section('content')
 	<h1 class="main-title">{{ $title }}</h1>
 	@if(isset($guests))
-		@foreach($guests as $guest)
-			<ul>
-				<li>{{ $guest->first_name }}</li>
-			</ul>
-		@endforeach
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($guests as $guest)
+					<tr>
+						<td>{{ $guest->first_name }}</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
 	@else
 		<div class="error-container">
 			<h2>I sense a disturbance in the force!</h2>
 			<p>Sorry buddy, an error occurred.</p>
 		</div>
 	@endif
+
+	<script>
+		$(document).ready( function () {
+	    	$('table').DataTable();
+		});
+	</script>
 @stop
