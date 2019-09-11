40896727b1731c951b5a47cc519b0f33b2554101
thomas-j92 <thomas.j@impactus.co.uk>
2019-09-11 20:57:21 +0100

    add main guest/additional guests as model attributes

--- app/Invite.php
+++ app/Invite.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Invite extends Model
 {
 	protected $fillable = ['day', 'night'];
+
+	protected $appends 	= [
+		'main_guest',
+		'additional_guests'
+	];
 	
     public function guests() {
     	return $this->hasMany('App\InviteGuests', 'invite_id', 'id');
@@ -14,5 +19,13 @@ class Invite extends Model
 
     public function plus_ones() {
     	return $this->hasMany('App\PlusOne', 'invite_id', 'id');
+    }
+
+    public function getMainGuestAttribute() {
+    	return $this->guests->where('type', 'main')->first();
+    }
+
+    public function getAdditionalGuestsAttribute() {
+    	return $this->guests->where('type', 'additional');
     }
 }
