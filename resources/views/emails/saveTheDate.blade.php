598ca8c5d24951bdc5cd829c20b1f0a773f3c060
Tom Jinks <thomas.j@impactus.co.uk>
2020-06-16 20:40:11 +0100

    std updates

--- resources/views/emails/saveTheDate.blade.php
+++ resources/views/emails/saveTheDate.blade.php
@@ -5,108 +5,139 @@
 @endsection
 
 @section('content')
-
+	
 	<div class="email-container">
-		<div class="email-row blue-bg" id="std-header">
-			<div class="email-col email-col-6">
-				<p>We're getting married</p>
 
-			</div>
-			<div class="email-col email-col-6">
-				<p>Jessica & Thomas</p>
-			</div>
+		<div class="email-body">
+
+			<p class="std-desc">Would like you to save our date, Wednesday 27th of October, 2021.</p>
+			
+			<table cellspacing="0" cellpadding="0" class="std-details">
+				<tr>
+					<td class="blue-bg">
+						<p class="date">Wednesday 27th | October 2021</p>
+
+						<div class="address">
+							<p class="address_1">The Mills Barn</p>
+							<p class="address_2">Alveley</p>
+							<p class="address_3">Near Bridnorth</p>
+							<p class="county">Shropshire</p>
+							<p class="postcode">WV15 6HL</p>
+						</div>
+					</td>
+				</tr>
+			</table>
 		</div>
-
-		<div class="email-row" id="std-subheading">
-			<div class="email-col email-col-3 mr-1 rose-gold" >
-				<img src="{{ asset('images/emails/rose-gold.jpeg') }}">
-			</div>
-			<div class="email-col email-col-9 blue-bg">
-				<p>Save our date</p>
-			</div>
-		</div>
-
-		<div class="email-row address_container" id="std-details">
-			<div class="email-col email-col-11 mr-1 blue-bg">
-				<p class="date">Wednesday 27th | October 2021</p>
-
-				<div class="address">
-					<p class="address_1">The Mills Barn</p>
-					<p class="address_2">Alveley</p>
-					<p class="address_3">Near Bridnorth</p>
-					<p class="county">Shropshire</p>
-					<p class="postcode">WV15 6HL</p>
-				</div>
-			</div>
-			<div class="email-col email-col-1">
-				<img src="{{ asset('images/emails/rose-gold.jpeg') }}">
-			</div>
-		</div>
+		
 	</div>
+	{{-- <div class="email-container">
+		<table cellspacing="0" cellpadding="0" class="std-heading">
+			<tr>
+				<td class="blue-bg" width="80%">
+					<p>We're getting married</p>
+					<p>Jessica & Thomas</p>
+				</td>
+				<td width="20%">
+					<img src="{{ asset('images/mill-logo.png') }}">
+				</td>
+			</tr>
+		</table>
 
+		<table cellspacing="0" cellpadding="0" class="std-wording">
+			<tr>
+				<td width="25%" class="img">
+					<img src="{{ asset('images/emails/rose-gold.jpeg') }}">
+				</td>
+				<td width="75%" class="blue-bg">
+					<p>Save our date</p>
+				</td>
+			</tr>
+		</table>
+
+		<table cellspacing="0" cellpadding="0" class="std-details">
+			<tr>
+				<td width="90%" class="blue-bg">
+					<p class="date">Wednesday 27th | October 2021</p>
+
+					<div class="address">
+						<p class="address_1">The Mills Barn</p>
+						<p class="address_2">Alveley</p>
+						<p class="address_3">Near Bridnorth</p>
+						<p class="county">Shropshire</p>
+						<p class="postcode">WV15 6HL</p>
+					</div>
+				</td>
+				<td width="10%" class="img">
+					<img src="{{ asset('images/emails/rose-gold.jpeg') }}">
+				</td>
+			</tr>
+		</table>
+	</div> --}}
 
 @endsection
 
 <style>
 	.email-container {
-		color: #FFF;
-	}
-	.email-row {
-		display: -webkit-box;
-	    display: -ms-flexbox;
-	    display: flex;
-	    -ms-flex-wrap: wrap;
-	    flex-wrap: wrap;
-		width: 100%;
-		margin-bottom: 1em;
-	}
-	.email-col {
+		background-image: url({{ asset('images/emails/rose-gold.jpeg') }});
+		height: 20em;
 		position: relative;
-		-ms-flex-preferred-size: 0;
-    flex-basis: 0;
-    -webkit-box-flex: 1;
-    -ms-flex-positive: 1;
-    flex-grow: 1;
 	}
-	.mr-1 {
-		margin-right: 1em;
+	.email-container .email-body {
+		background-color: rgb(15, 21, 33);
+		position: absolute;
+	    left: 1em;
+	    right: 1em;
+	    bottom: 1em;
+	    top: 1em;
 	}
-	.email-col-1 {
-		width: 8.333333%;
-	}
-	.email-col-3 {
-		width: 25%;
-	}
-	.email-col-6 {
-		width: 50%;
-		float:left;
-	}
-	.email-col-9 {
-		width: 75%;
-	}
-	.email-col-11 {
-		width: 91.666667%;
-	}
-	.blue-bg {
-		background-color: #121828;
-	}
-	.rose-gold img {
+	.email-container table {
 		width: 100%;
-		height: 6em;
 	}
-	.address_container img {
-		height: 100%;
-	}
-	#std-header .email-col p,
-	#std-subheading .email-col p {
-		padding: 1em;
+	.email-container p {
 		margin: 0;
 		text-align: center;
 	}
-	#std-subheading .email-col p {
-		width: 100%;
-	    position: absolute;
-	    top: 50%;
-	    transform: translate(0, -50%);
+	.std-desc {
+		padding-bottom: 2em;
 	}
+	/*.email-container p {
+		margin: 0;
+		color: #FFF;
+		font-family: "Trebuchet MS", Helvetica, sans-serif;
+	}
+	/*.email-container p {
+		margin: 0;
+		color: #FFF;
+		font-family: "Trebuchet MS", Helvetica, sans-serif;
+	}
+	.email-container table {
+		border: 0;
+		width: 100%;
+		margin-bottom: 1em;
+	}
+	.email-container .blue-bg {
+		background-color: #121828;
+	}
+	.email-container .std-heading p {
+		text-align: center;
+		padding: 0.5em 0em;
+	}
+	.email-container .std-wording p {
+		text-align: center;
+	}
+	.email-container .std-details p {
+		text-align: center;
+	}
+	.email-container .std-wording .img {
+		padding-right: 1em;
+	}
+	.email-container .std-wording img {
+		height: auto;
+	}
+	.email-container .std-details .img {
+		padding-left: 1em;
+	}
+	.email-container .std-details img {
+		height: 15em;
+	}*/
 </style>
